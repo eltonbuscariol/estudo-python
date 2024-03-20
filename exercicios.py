21e6ec857c47d425ca1477bd4c75b7ebf3f2fcb5
Elton <elton.buscariol@gmail.com>
2024-03-19 22:37:59 -0300

    exercicios

---
+++ exercicios.py
@@ -0,0 +1,17 @@
+'''
+Criar um dicionário de cadastro de games com as seguintes propriedades:
+
+* Nome
+* Produtora
+* Idade Minima
+* tipo
+* online
+
+Verificar se o nome está preenchido
+Verificar se a produtora foi informada
+Verificar se o game te idade maior que 5 anos
+Verificar se o tipo é corrida
+Verificar se o jogo é online
+
+'''
+
---
+++ loops.py
@@ -0,0 +1,17 @@
+'''
+Estruturas de repetição
+
+for = Para
+while = Enquanto
+'''
+
+for contador in range(0, 10):
+    print(contador)
+
+
+print('=' * 50)
+count = 0
+while count < 10:
+    print(count)
+    count = count + 1
+    
